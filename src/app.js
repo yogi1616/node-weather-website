8ccebb99523d0d3bf93123623d0b86a13070b582
yogi1616 <yogesh_barot07@yahoo.com>
2019-03-11 15:28:57 +0530

    pages modified

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title:'Weather App',
+        title:'Weather',
         name:'Yogesh Barot'
     })
 })
